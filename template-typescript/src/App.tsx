6d8c4641c06603e37a7a4fd272925b7b3f40c02b
Hiroki Takezawa <hiroki.takezawa@outlook.jp>
2019-01-06 12:30:39 +0900

    V2.1.3 (#1)
    * v2.1.3
    
    * turn on babelrc

--- template-typescript/src/App.tsx
+++ template-typescript/src/App.tsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Comp2 from 'comp2';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">YarnWS-CraApp1</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Comp2 />
       </div>
     );
   }
